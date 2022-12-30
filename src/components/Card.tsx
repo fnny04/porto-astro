62a614d10fc70e486bd808108216cf32120548a6
Fenny Oktaviani <fnny04@gmail.com>
2022-12-30 17:47:50 +0700

    Fenny Oktaviani | Updated Responsive Navbar & Page Project

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,3 +1,15 @@
-export default function Card() {
-  return <div>Hallo</div>;
+import { FaGithub } from "react-icons/fa";
+
+export default function Card(props: { text?: string }) {
+  const { text } = props;
+  return (
+    <>
+      <a href="https://github.com/fnny04?tab=repositories">
+        <div className="flex flex-col p-4 m-4 bg-gray-100 border-md rounded-md items-center ">
+          <h1>{text}</h1>
+          <FaGithub className="text-4xl m-2" />
+        </div>
+      </a>
+    </>
+  );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,29 +1,58 @@
-import Button from "./Button";
 import ThemeToggle from "./ThemeToogle";
+import { useState } from "react";
+import { RxHamburgerMenu } from "react-icons/rx";
 
 export default function Navbar() {
+  const [hamburgerOpen, sethamburgerOpen] = useState(false);
+  const toggleHamburger = () => {
+    sethamburgerOpen(!hamburgerOpen);
+  };
+
   return (
-    <header className="flex fixxed top-0 relative items-center w-full h-auto bg-gray-400 dark:bg-gray-700 justify-between p-4 text-white font-bold">
-      <figure>
-        <figcaption className="text-gray-600 dark:text-white ">
-          Fenny | Personal Website
-        </figcaption>
-      </figure>
-      <nav className="md:flex hidden gap-4  text-gray-600 dark:text-gray-200 items-center">
-        <a
-          className="hover:text-pink-400 hover:border-b-2 hover:border-pink-400 "
-          href="/"
-        >
-          Profile
-        </a>
-        <a className="hover:text-pink-400  " href="/about">
-          About
-        </a>
-        <a className="hover:text-pink-400  " href="/contact">
-          Project
-        </a>
-      </nav>
-      <ThemeToggle />
-    </header>
+    <>
+      <header className="flex fixed top-0 relative items-center w-full h-auto bg-gray-400 dark:bg-gray-700 justify-between p-4 text-white font-bold">
+        <RxHamburgerMenu
+          onClick={toggleHamburger}
+          className="md:hidden text-3xl cursor-pointer"
+        />
+
+        <figure>
+          <figcaption className="text-gray-600 dark:text-white ">
+            Fenny | Personal Website
+          </figcaption>
+        </figure>
+        <nav className="md:flex hidden gap-4  text-gray-600 dark:text-gray-200 items-center">
+          <a
+            className="hover:text-pink-400 hover:border-b-2 hover:border-pink-400 "
+            href="/"
+          >
+            Profile
+          </a>
+          <a className="hover:text-pink-400  " href="/about">
+            About
+          </a>
+          <a className="hover:text-pink-400  " href="/project">
+            Project
+          </a>
+        </nav>
+        <ThemeToggle />
+      </header>
+      {hamburgerOpen && (
+        <nav className="flex flex-col md:hidden bg-gray-400 dark:bg-gray-700 p-2 gap-4  text-gray-600 dark:text-gray-200 items-center">
+          <a
+            className="hover:text-pink-400 hover:border-b-2 hover:border-pink-400 "
+            href="/"
+          >
+            Profile
+          </a>
+          <a className="hover:text-pink-400  " href="/about">
+            About
+          </a>
+          <a className="hover:text-pink-400  " href="/project">
+            Project
+          </a>
+        </nav>
+      )}
+    </>
   );
 }
