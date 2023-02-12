efc42368ea2a5df6b6fb1c13f222718aa52fce0c
Fenny Oktaviani <fnny04@gmail.com>
2023-02-12 17:58:28 +0700

    Improvement Project Page

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,10 +1,12 @@
 import project from "../assets/Apotekqyu.png";
 import gallery from "../assets/gallery.png";
+import market from "../assets/VegetaMart.png";
 import web from "../assets/Webapp.png";
+import votsu from "../assets/votsu.png";
 
 export default function Project() {
   return (
-    <div className="grid grid-rows-1 md:grid-cols-3  gap-4 p-4">
+    <div className="grid grid-cols-1 md:grid-cols-3  gap-4 p-4">
       <div className="p-4 bg-white rounded-md">
         <a href="https://apotekqyu.netlify.app/">
           <img src={project} alt="" />
@@ -20,11 +22,16 @@ export default function Project() {
           </h1>
         </a>
       </div>
-      <div className="p-2 bg-white rounded-md">
-        <a href="https://gallery-app-rg.netlify.app/">
-          <img src={gallery} alt="" />
-          <h1 className="ml-14 text-gray-600">
-            https://gallery-app-rg.netlify.app/
+      <div className="p-4 bg-white rounded-md">
+        <a href="https://vegetamart.netlify.app/">
+          <img src={market} alt="" />
+          <h1 className="ml-14 m-4 text-gray-600">
+            https://vegetamart.netlify.app/
+          </h1>
+        </a>
+        <a href="https://github.com/fnny04/shopping-react">
+          <h1 className="ml-14 my-4 text-gray-600">
+            https://github.com/fnny04/shopping-react
           </h1>
         </a>
       </div>
@@ -40,6 +47,29 @@ export default function Project() {
         <a href="https://github.com/fnny04/Simply-Webapp-Funny">
           <h1 className="ml-14 my-4 text-gray-600">
             https://github.com/fnny04/Simply-Webapp-Funny
+          </h1>
+        </a>
+      </div>
+      <div className="p-2 bg-white rounded-md">
+        <a href="https://gallery-app-rg.netlify.app/">
+          <img src={gallery} alt="" />
+          <h1 className="ml-14 text-gray-600">
+            https://gallery-app-rg.netlify.app/
+          </h1>
+        </a>
+      </div>
+      <div className="p-4 bg-white rounded-md">
+        <a href="https://votsu.netlify.app/">
+          <img src={votsu} alt="" />
+        </a>
+        <a href="https://votsu.netlify.app//">
+          <h1 className="ml-14 my-4 text-gray-600">
+            https://votsu.netlify.app/
+          </h1>
+        </a>
+        <a href="https://github.com/psudevteam/votsu-fe">
+          <h1 className="ml-14 my-4 text-gray-600">
+            https://github.com/psudevteam/votsu-fe
           </h1>
         </a>
       </div>
